56d3d044f64340a52dc24f8c3c817a9e56c6ede7
Bart Duisters <bartduisters@bartduisters.com>
2022-03-04 22:10:03 +0100

    feat: style MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>Tour of Heroes</div>
-  <button @click="navigate('/')">Dashboard</button>
-  <button @click="navigate('/heroes')">Heroes</button>
+  <div class="layout-container">
+    <div class="title">Tour of Heroes</div>
+    <div class="button-container">
+      <button @click="navigate('/')">Dashboard</button>
+      <button @click="navigate('/heroes')">Heroes</button>
+    </div>
 
-  <router-view></router-view>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,3 +30,35 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+}
+
+.layout-container {
+  margin: 2rem;
+}
+
+.button-container {
+  display: flex;
+  gap: 0.25rem;
+}
+
+button {
+  background-color: #eeeeee;
+  border-radius: 0.25rem;
+  font-weight: 500;
+  border: none;
+  padding: 0.25rem 0.5rem;
+  color: #567868;
+
+  &:hover {
+    background-color: darken(#eeeeee, 10%);
+    color: #0096e8;
+    cursor: pointer;
+  }
+}
+</style>
