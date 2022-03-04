4303123bee83bf6c5371d17d1edfade769cbd6f5
Bart Duisters <bartduisters@bartduisters.com>
2022-03-04 01:03:14 +0100

    feat: navigation coupled to buttons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,28 @@
 <template>
   <div>Tour of Heroes</div>
-  <button>Dashboard</button>
-  <button>Heroes</button>
+  <button @click="navigate('/')">Dashboard</button>
+  <button @click="navigate('/heroes')">Heroes</button>
 
   <router-view></router-view>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+
+    const navigate = (path: string) => {
+      // void router.push('/); Navigate to Dashboard
+      // void router.push('/heroes'); Navigate to HeroList
+      void router.push(path);
+    };
+
+    return {
+      navigate,
+    };
+  },
+});
+</script>
