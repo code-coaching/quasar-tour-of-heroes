cb681e49138851e87ffa8e39c7fa9c6955c0f1a3
Bart Duisters <bartduisters@bartduisters.com>
2022-03-08 20:19:16 +0100

    feat: style dashboard.vue

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Dashboard works!</h1>
+  <div class="title">Top Heroes</div>
+
+  <div class="top-heroes">
+    <div class="top-hero">Narco</div>
+    <div class="top-hero">Bombasto</div>
+    <div class="top-hero">Celeritas</div>
+    <div class="top-hero">Magneta</div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+}
+
+.top-heroes {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+
+.top-hero {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #5f7d8c;
+  color: white;
+
+  height: 8rem;
+  width: 10rem;
+  font-weight: 600;
+  font-size: 1.1rem;
+
+  &:hover {
+    background-color: #eeeeee;
+    color: #5f7d8c;
+    cursor: pointer;
+  }
+}
+</style>
