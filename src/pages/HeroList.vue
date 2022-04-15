be91f2b5d883f04f3b88a90a2276da0cf505fe69
Bart Duisters <bartduisters@bartduisters.com>
2022-04-16 00:38:34 +0200

    feat: ref variable in herolist

--- src/pages/HeroList.vue
+++ src/pages/HeroList.vue
@@ -2,42 +2,52 @@
   <div class="title">My Heroes</div>
 
   <div class="hero-list">
-    <div class="hero">
-      <span class="hero-number">11</span>
-      <span class="hero-name">Mr. Nice</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">12</span> <span class="hero-name">Narco</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">13</span>
-      <span class="hero-name">Bombasto</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">14</span>
-      <span class="hero-name">Celeritas</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">15</span> <span class="hero-name">Magneta</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">16</span>
-      <span class="hero-name">RubberMan</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">17</span> <span class="hero-name">Dynama</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">18</span> <span class="hero-name">Dr IQ</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">19</span> <span class="hero-name">Magma</span>
-    </div>
-    <div class="hero">
-      <span class="hero-number">20</span> <span class="hero-name">Tornado</span>
+    <div
+      class="hero"
+      v-for="(hero, index) in heroes"
+      :key="index"
+      @click="onClickHero(hero)"
+    >
+      <span class="hero-number">{{ hero.number }}</span>
+      <span class="hero-name">{{ hero.name }}</span>
     </div>
   </div>
+
+  <div>{{ selectedHero }}</div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const selectedHero = ref();
+
+    const heroes = ref([
+      { number: 11, name: 'Mr. Nice' },
+      { number: 12, name: 'Narco' },
+      { number: 13, name: 'Bombasto' },
+      { number: 14, name: 'Celeritas' },
+      { number: 15, name: 'Magneta' },
+      { number: 16, name: 'RubberMan' },
+      { number: 17, name: 'Dynama' },
+      { number: 18, name: 'Dr IQ' },
+      { number: 19, name: 'Magma' },
+      { number: 20, name: 'Tornado' },
+    ]);
+
+    function onClickHero(hero: { number: number; name: string }) {
+      selectedHero.value = hero;
+    }
+
+    return {
+      heroes,
+      selectedHero,
+      onClickHero,
+    };
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 .title {
