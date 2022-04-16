0a1e9d010fa276e13a41086b3c1a53c3c55f2c78
Bart Duisters <bartduisters@bartduisters.com>
2022-04-17 00:12:22 +0200

    feat: HeroDetails initial setup

--- src/pages/HeroDetails.vue
+++ src/pages/HeroDetails.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Hero details works!</h1>
+  <div v-if="hero">
+    <div class="title">{{ hero.name }} details!</div>
+
+    <div>id: {{ hero.number }}</div>
+    <div>name: <input :value="hero.name" /></div>
+
+    <StyledButton>Back</StyledButton>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue';
+import StyledButton from 'components/StyledButton.vue';
+
+interface Hero {
+  number: number;
+  name: string;
+}
+
+export default defineComponent({
+  components: {
+    StyledButton,
+  },
+  setup() {
+    const hero: Ref<Hero> = ref({ number: 15, name: 'Magneta' }) as Ref<Hero>;
+
+    return {
+      hero,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
