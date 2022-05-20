0b03ce0068d659f1b4a485a03c8116b3e3546146
Bart Duisters <bartduisters@bartduisters.com>
2022-05-20 23:44:57 +0200

    refactor: styling login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,28 +1,32 @@
 <template>
-  <input type="email" v-model="user.email" />
-  <input type="password" v-model="user.password" />
-  <button @click="login()">Login</button>
+  <div class="login-container">
+    <div class="login-fields">
+      <input type="email" v-model="user.email" />
+      <input type="password" v-model="user.password" />
+    </div>
+    <StyledButton @click="login()">Login</StyledButton>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
 import { useAuth } from 'src/services/auth.service';
+import StyledButton from 'src/components/StyledButton.vue';
 
 export default defineComponent({
+  components: { StyledButton },
   setup() {
     const { login: authenticate } = useAuth();
     const user = reactive({
       email: '',
       password: '',
     });
-
     const login = () => {
       authenticate(user.email, user.password).finally(() => {
         user.email = '';
         user.password = '';
       });
     };
-
     return {
       login,
       user,
@@ -30,3 +34,19 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+.login-container {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 20rem;
+}
+
+.login-fields {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+</style>
