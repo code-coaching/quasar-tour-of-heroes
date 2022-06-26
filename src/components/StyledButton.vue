de910a5006e094d3a3cda361d64279dec5adc924
Bart Duisters <bartduisters@bartduisters.com>
2022-06-26 22:23:24 +0200

    refactor: remove hover states of StyledButton - let Quasar handle it

--- src/components/StyledButton.vue
+++ src/components/StyledButton.vue
@@ -34,31 +34,15 @@ button {
   border: none;
   padding: 0.25rem 0.5rem;
   color: #567868;
-
-  &:hover {
-    background-color: darken(#eeeeee, 10%);
-    color: #0096e8;
-    cursor: pointer;
-  }
 }
 
 .primary {
   background-color: var(--q-primary);
   color: white;
-
-  &:hover {
-    background-color: darken($primary, 5%);
-    color: white;
-  }
 }
 
 .negative {
   background-color: var(--q-negative);
   color: white;
-
-  &:hover {
-    background-color: darken($negative, 5%);
-    color: white;
-  }
 }
 </style>
